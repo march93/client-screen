f5065e947bb71f2221f381e164d28d0a9686c099
Marc Ho <marc.ho380@gmail.com>
2023-12-27 08:57:05 -0800

    Update server file to use config service

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import * as dotenv from 'dotenv';
+import { PORT } from './config';
+
 import App from './app';
 
-dotenv.config();
-
-const port = process.env.PORT || 3000;
+const port = PORT || 3000;
 
 const app = new App();
 app.server.listen(port);
